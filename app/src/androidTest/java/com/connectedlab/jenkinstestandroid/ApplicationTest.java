243713f9d231182cde01b965b0347cb7e4f39dbc
//yk <yashk.23@gmail.com>
2015-02-03 10:42:08 -0500

    Instrumentation

--- app/src/androidTest/java/com/connectedlab/jenkinstestandroid/ApplicationTest.java
+++ app/src/androidTest/java/com/connectedlab/jenkinstestandroid/ApplicationTest.java
@@ -2,15 +2,12 @@ package com.connectedlab.jenkinstestandroid;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.InstrumentationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
-    }
-
+public class ApplicationTest extends InstrumentationTestCase {
     public void testFail() {
         assertEquals(false, true);
     }
